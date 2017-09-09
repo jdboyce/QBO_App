07cecf6ffa9d77f5ef603df1871276d8d713364a
Boyce <joshuadboyce@gmail.com>
2017-09-08 19:43:17 -0500

    Created Project and Added Nuget Package

---
+++ QBO_App_V1/QBO_App_V1/Controllers/QBOController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QBO_App_V1.Controllers
+{
+    public class QBOController : Controller
+    {
+        // GET: QBO
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
